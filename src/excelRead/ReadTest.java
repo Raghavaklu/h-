ec37c683a42f3c564e2a0799cf1e2d5235c8427d
kanakaratnam <kanakaratnam4a2@gmail.com>
2019-12-23 14:57:35 +0530

    first commit

---
+++ src/excelRead/ReadTest.java
@@ -0,0 +1,28 @@
+package excelRead;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import jxl.Workbook;
+import jxl.write.Label;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import jxl.write.WriteException;
+import jxl.write.biff.RowsExceededException;
+
+public class ReadTest 
+{
+	public static void main(String[] args) throws IOException, RowsExceededException, WriteException
+	{
+	WritableWorkbook wb=Workbook.createWorkbook(new File("D:\\test Sheet.xls"));
+	WritableSheet ws=wb.createSheet("test Sheeet", 0);
+	Label label=new Label(0, 0, "employee");
+	
+	
+	
+	}
+	
+}
